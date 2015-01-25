f16688eb4fbab0a21539ca0687ed47ddcf806417
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-01-25 12:40:12 +0100

    Update french translation

--- app/translations/mellowplayer_fr.ts
+++ app/translations/mellowplayer_fr.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR">
 <context>
+    <name>DeezerPlugin</name>
+    <message>
+        <location filename="../../plugins/deezer/deezer.cpp" line="37"/>
+        <source>&lt;p&gt;&lt;strong&gt;Deezer&lt;/strong&gt; is a web-based music streaming service. It allows users to listen to music content from record labels including EMI, Sony, Universal Music Group, and Warner Music Group on various devices online or offline. Created in Paris, France, Deezer currently has 35 million licensed tracks in its library, over 30,000 radio channels, 16 million monthly active users, and 5 million paid subscribers as of 6 November 2013.&lt;/p&gt;&lt;p&gt;&lt;em&gt;Source:&lt;a href=&quot;http://en.wikipedia.org/wiki/Deezer&quot;&gt;Deezer on Wikipedia&lt;/a&gt;,&lt;a href=&quot;http://deezer.com&quot;&gt;Official website&lt;/a&gt;&lt;/em&gt;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;strong&gt;Deezer&lt;/strong&gt; est un service d&apos;écoute de musique en streaming à la demande permettant d’écouter des artistes, titres, et albums, ainsi que de créer et d&apos;échanger des sélections grâce à un catalogue de 35 millions de titres, issus de tous les genres musicaux.&lt;/p&gt;&lt;p&gt;Lors de sa création en août 2007, le site était entièrement gratuit ; il se met à proposer deux services payants à partir de novembre 2009. Désormais le service gratuit a des publicités audio toutes les quinze minutes selon Jonathan Benassaya (cofondateur) début mars 2009. Depuis le 9 février 2009, l&apos;écoute sans limite nécessite une authentification. En février 2010, 46 personnes y sont salariées, et une centaine en octobre 20113. À partir du 6 juin 2011, l&apos;écoute de titres en étant connecté à son compte gratuit est limitée en durée d&apos;écoute. Le catalogue de la version payante est plus grand qu&apos;en gratuit mais l&apos;abonnement ne donne pas accès à toutes les musiques commercialisées. En décembre 2011, il rassemblait 20 millions d&apos;utilisateurs dont 1,4 million d&apos;abonnés à Deezer Premium. En septembre 2011, Deezer lance son déploiement à l&apos;international avec l&apos;ouverture de Deezer UK et signe un partenariat mondial avec Facebook pour l&apos;intégration de Deezer au sein de ce réseau social. En octobre 2012, avec un catalogue de 20 millions de titres, il rassemble 26 millions d&apos;utilisateurs dont 2 millions d&apos;abonnés.&lt;/p&gt;&lt;p&gt;Deezer s&apos;est très internationalisé en 2012. Le 15 mai 2013, Deezer est présent dans 221 territoires. Deezer est absent en Chine, en Inde, au Japon… Il est disponible en 9 langues : français, anglais, allemand, espagnol, russe, indonésien, coréen, portugais et italien. Il utilise l&apos;HTML5.&lt;/p&gt;&lt;p&gt;Le groupe a annoncé le 10 septembre 2014 qu&apos;il se lançait à partir du 15 septembre aux États-Unis. Il lance l&apos;offre &quot;Deezer Elite&quot; en partenariat avec Sonos, le leader mondial des systèmes audio sans fil connectés. Il s&apos;agit de la première proposition du groupe français d&apos;un son en très haute résolution (HRA).&lt;/p&gt;&lt;p&gt;&lt;em&gt;Source:&lt;a href=&quot;http://fr.wikipedia.org/wiki/Deezer&quot;&gt;Deezer sur Wikipedia&lt;/a&gt;,&lt;a href=&quot;http://deezer.com&quot;&gt;Site web officiel&lt;/a&gt;&lt;/em&gt;&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
     <name>DialogPreferences</name>
     <message>
         <location filename="../dlg_preferences.ui" line="14"/>
@@ -167,17 +175,17 @@ Voulez-vous redémarrer maintenant?</translation>
     </message>
     <message>
         <location filename="../dlgpreferences.cpp" line="198"/>
-        <source>This will remove all cookies stored by Mellow Player.
+        <source>This will remove all cookies stored by MellowPlayer.
 Are you sure you want to clear all cookies?</source>
-        <translation>Cette action va nettoyer les cookies stockées par Mellow Player.
-Etes-vous sur de vouloir supprimer toutes les cookies?</translation>
+        <translation>Cette action va supprimer tous les cookies stockés par MellowPlayer.
+Etes-vous sûr de vouloir supprimer tous les cookies?</translation>
     </message>
     <message>
         <location filename="../dlgpreferences.cpp" line="213"/>
-        <source>This will remove all settings stored by Mellow Player.
+        <source>This will remove all settings stored by MellowPlayer.
 Are you sure you want to erase all settings?</source>
-        <translation>Cette action va nettoyer les paramètres de l&apos;application.
-Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
+        <translation>Cette action va supprimer toutes les paramètres de l&apos;application.
+Etes-vous sûr de vouloir supprimer les paramètres de l&apos;application?</translation>
     </message>
     <message>
         <location filename="../dlgpreferences.cpp" line="212"/>
@@ -190,7 +198,7 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
     <message>
         <location filename="../../plugins/grooveshark/grooveshark.cpp" line="37"/>
         <source>&lt;p&gt;&lt;strong&gt;Grooveshark&lt;/strong&gt; is an international online music search engine, music streaming service and music recommendation web software application, allowing users to search for, stream, and upload music that can be played immediately or added to a playlist. An optional paid subscription adds additional functionality and removes advertisements.&lt;/p&gt;&lt;p&gt;&lt;em&gt;Source:&lt;a href=&quot;http://en.wikipedia.org/wiki/Grooveshark&quot;&gt;Grooveshark on Wikipedia&lt;/a&gt;,&lt;a href=&quot;http://grooveshark.com&quot;&gt;Official website&lt;/a&gt;&lt;/em&gt;&lt;/p&gt;</source>
-        <translation>&lt;p&gt;&lt;strong&gt;Grooveshark&lt;/strong&gt; est un service de recherche, de recommendation et de streaming de musique en ligne, permettant à ses utilisateurs de rechercher et de téléverser de la musique qui peut être jouée directement ou être ajoutée à une liste de lecture. Un abonnement payant optionnel permet de retirer les publicités et d&apos;avoir accès à des fontionnalités supplémentaires.&lt;/p&gt;&lt;p&gt;&lt;em&gt;Source:&lt;a href=&quot;http://en.wikipedia.org/wiki/Grooveshark&quot;&gt;Grooveshark sur Wikipedia&lt;/a&gt;,&lt;a href=&quot;http://grooveshark.com&quot;&gt;Site web officiel&lt;/a&gt;&lt;/em&gt;&lt;/p&gt;</translation>
+        <translation>&lt;p&gt;&lt;strong&gt;Grooveshark&lt;/strong&gt; est un service de recherche, de recommendation et de streaming de musique en ligne, permettant à ses utilisateurs de rechercher et de téléverser de la musique qui peut être jouée directement ou être ajoutée à une liste de lecture. Un abonnement payant optionnel permet de retirer les publicités et d&apos;avoir accès à des fontionnalités supplémentaires.&lt;/p&gt;&lt;p&gt;&lt;em&gt;Source:&lt;a href=&quot;http://fr.wikipedia.org/wiki/Grooveshark&quot;&gt;Grooveshark sur Wikipedia&lt;/a&gt;,&lt;a href=&quot;http://grooveshark.com&quot;&gt;Site web officiel&lt;/a&gt;&lt;/em&gt;&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
@@ -217,16 +225,6 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
 </context>
 <context>
     <name>MainWindow</name>
-    <message>
-        <location filename="../mainwindow.ui" line="14"/>
-        <source>Mellow Player</source>
-        <translation>Mellow Player</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="53"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;img src=&quot;:/icons/mellowplayer.png&quot; width=128 height=128/&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:22pt; font-weight:600;&quot;&gt;Welcome to Mellow Player!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-style:italic;&quot;&gt;Please, select a music streaming service.&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;img src=&quot;:/icons/mellowplayer.png&quot; width=128 height=128/&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:22pt; font-weight:600;&quot;&gt;Bienvenue dans Mellow Player!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-style:italic;&quot;&gt;Veuillez choisir un service de musique en ligne, svp...&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
     <message>
         <location filename="../mainwindow.ui" line="136"/>
         <source>Preferences</source>
@@ -273,6 +271,11 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
         <translation>Media Previous</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="363"/>
+        <source>&amp;About MellowPlayer</source>
+        <translation>&amp;A propos de MellowPlayer</translation>
+    </message>
+    <message>
         <location filename="../mainwindow.ui" line="366"/>
         <source>F1</source>
         <translation>F1</translation>
@@ -282,6 +285,16 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
         <location filename="../mainwindow.ui" line="291"/>
         <source>Select a music streaming service</source>
         <translation>Choisissez un service de musique en ligne</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MellowPlayer</source>
+        <translation>MellowPlayer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="53"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;img src=&quot;:/icons/mellowplayer.png&quot; width=&quot;128&quot; height=&quot;128&quot;/&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:22pt; font-weight:600;&quot;&gt;Welcome to MellowPlayer!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-style:italic;&quot;&gt;Please, select a music streaming service.&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;img src=&quot;:/icons/mellowplayer.png&quot; width=&quot;128&quot; height=&quot;128&quot;/&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:22pt; font-weight:600;&quot;&gt;Bienvenue dans MellowPlayer!&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt; font-style:italic;&quot;&gt;Veuillez choisir un service de musique en ligne.&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="241"/>
@@ -334,11 +347,6 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
         <translation>Piste &amp;précédente</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="363"/>
-        <source>&amp;About Mellow Player</source>
-        <translation>&amp;A propos de MellowPlayer</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="375"/>
         <source>&amp;Report a bug</source>
         <translation>&amp;Rapporter un bug</translation>
@@ -374,51 +382,46 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
         <translation>A propos de &amp;Qt</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="143"/>
+        <location filename="../mainwindow.cpp" line="144"/>
         <source>Minimizing to system tray</source>
         <translation>Minimisation dans la barre des tâches</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="144"/>
+        <location filename="../mainwindow.cpp" line="145"/>
         <source>The program will keep running in the system tray. To terminate the program, choose &lt;b&gt;Quit&lt;/b&gt; in the context menu of the system tray entry.</source>
         <translation>Le programme va continuer de fonctionner dans la barre des tâches. Pour terminer le programme, choississez &lt;b&gt;Quitter&lt;/b&gt; dans le menu contextuel de l&apos;icône de la barre des tâches.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="319"/>
-        <location filename="../mainwindow.cpp" line="339"/>
-        <location filename="../mainwindow.cpp" line="341"/>
+        <location filename="../mainwindow.cpp" line="320"/>
+        <location filename="../mainwindow.cpp" line="335"/>
+        <location filename="../mainwindow.cpp" line="337"/>
         <source>Play</source>
         <translation>Lecture</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="324"/>
+        <location filename="../mainwindow.cpp" line="325"/>
         <source>Pause</source>
         <translation>Pause</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="329"/>
-        <source>Loading, please wait...</source>
-        <translation>Chargement en cours, veuillez patienter...</translation>
+        <location filename="../mainwindow.cpp" line="375"/>
+        <source>&lt;h3&gt;MellowPlayer %1&lt;/h3&gt;Built on %2 at %3 (%4, %5 bit)&lt;br/&gt;&lt;br/&gt;Copyright 2015 Colin Duquesnoy. All rights reserved.&lt;br/&gt;&lt;br/&gt;The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.&lt;br/&gt;</source>
+        <translation>&lt;h3&gt;MellowPlayer %1&lt;/h3&gt;Compilé le %2 à %3 (%4, %5 bit)&lt;br/&gt;&lt;br/&gt;Copyright 2015 Colin Duquesnoy. Tous droits réservés.&lt;br/&gt;&lt;br/&gt;Le programme est fourni TEL QUEL sans GARANTIE D&apos;AUCUNE SORTE, Y COMPRIS DE GARANTIE DE LA CONCEPTION, DE QUALITÉ MARCHANDE ET D&apos;ADÉQUATION À UN USAGE PARTICULIER.&lt;br/&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="379"/>
-        <source>&lt;h3&gt;Mellow Player %1&lt;/h3&gt;Built on %2 at %3 (%4, %5 bit)&lt;br/&gt;&lt;br/&gt;Copyright 2015 Colin Duquesnoy. All rights reserved.&lt;br/&gt;&lt;br/&gt;The program is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.&lt;br/&gt;</source>
-        <translation>&lt;h3&gt;Mellow Player %1&lt;/h3&gt;Compilé le %2 à %3 (%4, %5 bit)&lt;br/&gt;&lt;br/&gt;Copyright 2015 Colin Duquesnoy. Tous droits réservés.&lt;br/&gt;&lt;br/&gt;Le programme est fourni TEL QUEL sans GARANTIE D&apos;AUCUNE SORTE, Y COMPRIS DE GARANTIE DE LA CONCEPTION, DE QUALITÉ MARCHANDE ET D&apos;ADÉQUATION À UN USAGE PARTICULIER.&lt;br/&gt;</translation>
+        <location filename="../mainwindow.cpp" line="388"/>
+        <source>About MellowPlayer</source>
+        <translation>A propos de MellowPlayer</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="392"/>
-        <source>About Mellow Player</source>
-        <translation>A propos de Mellow Player</translation>
+        <location filename="../mainwindow.cpp" line="409"/>
+        <source>Are you sure you want to exit MellowPlayer?</source>
+        <translation>Etes-vous sûr de vouloir quitter MellowPlayer?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="411"/>
+        <location filename="../mainwindow.cpp" line="408"/>
         <source>Confirm quit</source>
         <translation>Confirmer la sortie</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="412"/>
-        <source>Are you sure you want to exit Mellow Player?</source>
-        <translation>Etes-vous sûr de vouloir quitter Mellow Player?</translation>
     </message>
 </context>
 <context>
@@ -440,12 +443,12 @@ Etes-vous sûre de vouloir nettoyer tous les paramètres?</translation>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../dlgselectservice.cpp" line="63"/>
+        <location filename="../dlgselectservice.cpp" line="69"/>
         <source>&lt;h1&gt;%1&lt;/h1&gt;&lt;h2&gt;Description&lt;/h2&gt;%2&lt;h2&gt;Service information&lt;/h2&gt;&lt;p&gt;&lt;strong&gt;Maintainer: &lt;/strong&gt;&lt;a href=%3&gt;%4&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version: &lt;/strong&gt;%5&lt;/p&gt;</source>
         <translation>&lt;h1&gt;%1&lt;/h1&gt;&lt;h2&gt;Description&lt;/h2&gt;%2&lt;h2&gt;Information à propos du service&lt;/h2&gt;&lt;p&gt;&lt;strong&gt;Mainteneur: &lt;/strong&gt;&lt;a href=%3&gt;%4&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version: &lt;/strong&gt;%5&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../../lib/src/song.cpp" line="52"/>
+        <location filename="../../lib/src/song.cpp" line="47"/>
         <source>%1 by %2 from %3</source>
         <translation>%1 de %2 sur %3</translation>
     </message>
