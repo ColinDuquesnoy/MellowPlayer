4acdc012b39fa131c6e4c99429bd1ac50f1bbcaa
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-04-13 21:37:12 +0200

    Add support for gnome settings daemon (MediaKeys)

--- plugins/gnomemediakeys/gnomemediakeys.h
+++ plugins/gnomemediakeys/gnomemediakeys.h
@@ -1,4 +1,5 @@
 //---------------------------------------------------------
+//
 // This file is part of MellowPlayer.
 //
 // MellowPlayer is free software: you can redistribute it and/or modify
@@ -13,34 +14,40 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with MellowPlayer.  If not, see <http://www.gnu.org/licenses/>.
+//
 //---------------------------------------------------------
 
-#include "application.h"
-#include <QApplication>
-#include <QtPlugin>
+#include <QObject>
+#include <mellowplayer.h>
+
+class QDBusPendingCallWatcher;
+class OrgGnomeSettingsDaemonMediaKeysInterface;
 
 
-//---------------------------------------------------------
-// Add plugins here
-Q_IMPORT_PLUGIN(SoundcloudPlugin)
-Q_IMPORT_PLUGIN(MixcloudPlugin)
-Q_IMPORT_PLUGIN(DeezerPlugin)
-Q_IMPORT_PLUGIN(GroovesharkPlugin)
-Q_IMPORT_PLUGIN(HotkeysPlugin)
-Q_IMPORT_PLUGIN(NotificationsPlugin)
+// Adapted from Clementine's gnomeglobalshortcutbackend
+class GnomemediakeysPlugin :
+        public QObject,
+        public IExtension
+{
+    Q_OBJECT
 
-#ifndef Q_OS_WIN
-Q_IMPORT_PLUGIN(GroovesharkmobilePlugin)
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID IExtension_iid
+                      FILE "gnomemediakeys.json")
 #endif
+    Q_INTERFACES(IExtension)
 
-#ifdef Q_OS_LINUX
-Q_IMPORT_PLUGIN(Mpris2Plugin)
-Q_IMPORT_PLUGIN(GnomemediakeysPlugin)
-#endif
+public:
+    explicit GnomemediakeysPlugin(QObject* parent=NULL);
 
+    void setup();
+    void teardown();
+    const PluginMetaData& metaData() const;
 
-//---------------------------------------------------------
-int main(int argc, char *argv[]){
-    MellowPlayerApp a(argc, argv);
-    return a.exec();
-}
+private slots:
+    void registerFinished(QDBusPendingCallWatcher* watcher);
+    void gnomeMediaKeyPressed(const QString& _, const QString& key);
+private:
+    OrgGnomeSettingsDaemonMediaKeysInterface* m_interface;
+    bool m_connected;
+};
--- plugins/gnomemediakeys/gnomemediakeys.pro
+++ plugins/gnomemediakeys/gnomemediakeys.pro
@@ -17,19 +17,16 @@
 #
 #----------------------------------------------------------
 
-TEMPLATE = subdirs
+# This extension is built on GNU/Linux only!
+TEMPLATE      = lib
+TARGET        = mpp_gnomemediakeys
+CONFIG       += plugin static
+QT           += dbus gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+HEADERS       = gnomemediakeys.h
+SOURCES       = gnomemediakeys.cpp
+INCLUDEPATH  += ../../lib/include
+LIBS         += -L../../lib -lmellowplayer
+DESTDIR       = ../../app
 
-unix{
-    SUBDIRS += groovesharkmobile
-}
-unix:!macx {
-    SUBDIRS += mpris2
-    SUBDIRS += gnomemediakeys
-}
-
-SUBDIRS += grooveshark
-SUBDIRS += hotkeys
-SUBDIRS += notifications
-SUBDIRS += deezer
-SUBDIRS += mixcloud
-SUBDIRS += soundcloud
+DBUS_INTERFACES += org.gnome.SettingsDaemon.MediaKeys.xml
