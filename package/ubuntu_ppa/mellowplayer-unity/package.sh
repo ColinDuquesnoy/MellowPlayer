5b4131b6dbd2e9dcf49b1bb403a49de55c0bf658
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-04-15 09:15:09 +0200

    Fix build rules and rename ppa package to mellowplayer-unity
    There are still a lots of lintian warnings to fix. There should also be some other flavours: mellowplayer-kde (KDE Sc and Plasma 5) and mellowplayer (for other desktops)
    
    All those flavours should also conflict with each others

---
+++ package/ubuntu_ppa/mellowplayer-unity/package.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+
+package=mellowplayer-unity
+email=colin.duquesnoy@gmail.com
+version=1.3.0
+gpg_key=`cat gpg_key`
+echo "gpg key: ${gpg_key}"
+
+
+function wait_continue(){
+    echo "Press a key to continue"
+    read mot
+}
+
+function configure(){
+    rm debian/changelog
+	echo "-- configuring changelog:"
+        release=$1
+	sed "s/@release@/${release}/g" changelog.in > debian/changelog
+	cat debian/changelog
+	cp debian/changelog ${package}-${version}/debian/changelog
+}
+
+function clean(){
+	rm debian/changelog
+	rm *.build
+	rm *.dsc
+	rm *.changes
+	rm *.upload
+}
+
+function clean_all(){
+	clean
+	rm -rf ${package}-${version}
+	rm *.tar.gz
+}
+
+function setup_upstream_tarball(){
+	echo "-- setting up upstream tarball:"
+	tar -czf mellowplayer.tar.gz ../../../../MellowPlayer/
+	tar -xf mellowplayer.tar.gz
+	cp -R debian MellowPlayer
+	rm -rf MellowPlayer/package
+	rm -rf MellowPlayer/.git
+	mv MellowPlayer ${package}-${version}
+	tar -czf ${package}_${version}.orig.tar.gz ${package}-${version}
+}
+
+function make_source_package(){
+	echo "---- making package for $release"
+	dh_make -s -e ${email} -f ../${package}_${version}.orig.tar.gz
+	# debuild -us -uc
+	debuild -S -sa -k${gpg_key}
+}
+
+
+setup_upstream_tarball
+for release in 'trusty' 'utopic' 'vivid'
+do 
+	clean
+	configure ${release}
+	pushd ${package}-${version}
+    make_source_package
+	popd
+	wait_continue
+	dput ppa:colin-duquesnoy/experimental *.changes	
+	wait_continue
+done
+
+clean_all
+
