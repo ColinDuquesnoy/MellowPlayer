eabb7a18021c8a9c0ddd88eed9de13167ee10b74
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-01-14 19:51:23 +0100

    Add basic QML app
    Fix #33

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char * argv[])
 {
     QGuiApplication qtApp(argc, argv);
     QQmlApplicationEngine qmlApplicationEngine;
+    qmlApplicationEngine.load(QUrl(QLatin1String("qrc:/qml/main.qml")));
 
     return qtApp.exec();
 }
---
+++ src/main/qml/main.qml
@@ -0,0 +1,8 @@
+import QtQuick 2.7
+import QtQuick.Controls 2.0
+
+ApplicationWindow {
+    visible: true
+    visibility: "Maximized"
+    title: "MellowPlayer"
+}
