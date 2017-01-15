d756906b6e54d5f8b00dbfc0ad50e02c7d95269e
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-01-15 14:49:42 +0100

    Add a webview to main qml to check flash is correctly detected
    Currently show deezer

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtWebEngine>
 
 int main(int argc, char * argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication qtApp(argc, argv);
-    QQmlApplicationEngine qmlApplicationEngine;
+
+    QtWebEngine::initialize();
+
+    QQmlApplicationEngine qmlApplicationEngine;    
     qmlApplicationEngine.load(QUrl(QLatin1String("qrc:/qml/main.qml")));
 
     return qtApp.exec();
--- src/qml/main.qml
+++ src/qml/main.qml
@@ -1,8 +1,17 @@
 import QtQuick 2.7
 import QtQuick.Controls 2.0
+import QtWebEngine 1.3
+
 
 ApplicationWindow {
     visible: true
     visibility: "Maximized"
     title: "MellowPlayer"
+
+    WebEngineView {
+        id: webEngineView
+        url: "https://www.deezer.com/"
+        anchors.fill: parent
+        settings.pluginsEnabled : true
+    }
 }
