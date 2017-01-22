720b77487301693e6f52bb63b44b2b7bb46b97f8
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-01-22 12:34:28 +0100

    Remove unneeded submodule update
    We don't have anysubmodules in this project.

--- scripts/env-setup/linux/archlinux-env-setup.sh
+++ scripts/env-setup/linux/archlinux-env-setup.sh
@@ -2,4 +2,3 @@
 
 pacman --noconfirm --quiet -Syyu
 pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine cmake git
-git submodule update --init --recursive
--- scripts/env-setup/linux/fedora-env-setup.sh
+++ scripts/env-setup/linux/fedora-env-setup.sh
@@ -2,4 +2,3 @@
 
 dnf -y --quiet update
 dnf -y --quiet install qt5-qtquickcontrols2-devel qt5-qtwebengine-devel gcc-c++ cmake git lcov findutils
-git submodule update --init --recursive
