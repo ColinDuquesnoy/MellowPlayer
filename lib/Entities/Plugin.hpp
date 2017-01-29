050e45ce64cd2c2c42e604b5facccdb94670a611
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-01-29 20:15:31 +0100

    #37 Add Entitties::Plugin

---
+++ lib/Entities/Plugin.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <Common/Macros.hpp>
+#include <QtCore/QString>
+#include <QtGui/QImage>
+
+BEGIN_MELLOWPLAYER_NAMESPACE(Entities)
+
+/**
+ * @brief The plugin structure holds the data of a streaming service integration plugin.
+ */
+struct Plugin {
+    /**
+     * @brief Name of the plugin's author.
+     */
+    QString author;
+
+    /**
+     * @brief Url of the plugin's author's website.
+     */
+    QString authorWebsite;
+
+    /**
+     * @brief Color of the plugin (affects the color of the application toolbar).
+     *
+     * @remarks Leave empty to use the default toolbar color.
+     */
+    QString color;
+
+    /**
+     * @brief Logo of the plugin/service.
+     */
+    QImage logo;
+
+    /**
+     * @brief The name of the plugin.
+     */
+    QString name;
+
+    /**
+     * @brief The url of the streaming service
+     */
+    QString url;
+
+    /**
+     * @brief The javascript code of the plugin.
+     */
+    QString script;
+
+    /**
+     * @brief The version of the plugin.
+     */
+    QString version;
+
+    Plugin()
+        : author(""),
+          authorWebsite(""),
+          color(""),
+          logo(),
+          name(""),
+          url(""),
+          script(""),
+          version("") {
+
+    }
+};
+
+END_MELLOWPLAYER_NAMESPACE
---
+++ tests/Entities/PluginTests.cpp
@@ -0,0 +1,18 @@
+#include <catch.hpp>
+#include <Entities/Plugin.hpp>
+
+USE_MELLOWPLAYER_NAMESPACE(Entities)
+using namespace std;
+
+TEST_CASE("Plugin.DefaultCtor", "[Plugin]") {
+    Plugin plugin;
+
+    REQUIRE(plugin.author == "");
+    REQUIRE(plugin.authorWebsite == "");
+    REQUIRE(plugin.color == "");
+    REQUIRE(plugin.logo.isNull());
+    REQUIRE(plugin.name == "");
+    REQUIRE(plugin.url == "");
+    REQUIRE(plugin.script == "");
+    REQUIRE(plugin.version == "");
+}
