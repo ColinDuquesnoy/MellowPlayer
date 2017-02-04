86ae97892141aa49ccf6cb2f13481e0d88b8bea0
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-02-04 14:47:45 +0100

    CI: New stages setup
    - commit build: build in debug mode and run all tests + measure coverage on archlinux
    - full build: TO BE IMPLEMENT: build in release mode, run all tests and build a platform specific package if possible
    - maybe a release-build that upload the package to gitlab tags.

--- scripts/env-setup/linux/archlinux-env-setup.sh
+++ scripts/env-setup/linux/archlinux-env-setup.sh
@@ -2,3 +2,9 @@
 
 pacman --noconfirm --quiet -Syyu
 pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine cmake git
+
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/lcov.tar.gz
+tar -xvf lcov.tar.gz
+pushd lcov
+makepkg -si
+popd
