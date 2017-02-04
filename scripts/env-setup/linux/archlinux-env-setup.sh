2e3e1ee0f33e44e9cb11862ff95898dc18c7ba75
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-02-04 19:34:38 +0100

    CI: add missing qt5-svg package to env-setup scripts

--- scripts/env-setup/linux/archlinux-env-setup.sh
+++ scripts/env-setup/linux/archlinux-env-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pacman --noconfirm --quiet -Syyu
-pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine cmake git xorg-server-xvfb
+pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine qt5-svg cmake git xorg-server-xvfb
 
 curl -L -O  http://downloads.sourceforge.net/ltp/lcov-1.13.tar.gz
 tar -xvf lcov-1.13.tar.gz
