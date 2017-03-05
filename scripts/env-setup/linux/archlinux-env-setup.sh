ad9076113db28575cbd549a07c6b2c2cc8600c0d
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-03-05 18:07:58 +0100

    Add lcov archive to 3rdparty folder, avoid download
    Fix Recv failure: Connection reset by peer on Gitlab-CI

--- scripts/env-setup/linux/archlinux-env-setup.sh
+++ scripts/env-setup/linux/archlinux-env-setup.sh
@@ -3,8 +3,7 @@
 pacman --noconfirm --quiet -Syyu
 pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine qt5-svg cmake git xorg-server-xvfb
 
-curl -L -O  http://downloads.sourceforge.net/ltp/lcov-1.13.tar.gz
-tar -xvf lcov-1.13.tar.gz
+tar -xvf 3rdparty/lcov-1.13.tar.gz
 pushd lcov-1.13
 make PREFIX="/usr" install
 popd
