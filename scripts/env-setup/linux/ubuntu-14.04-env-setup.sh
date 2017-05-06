c6f8f8b27393f7ab38e4b2002bce3eda58e9ef48
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-05-06 16:58:46 +0200

    Add travis ci config file

---
+++ scripts/env-setup/linux/ubuntu-14.04-env-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Setup dev environment for Ubuntu 14.04
+sudo apt-add-repository ppa:beineri/opt-qt58-trusty -y
+sudo apt-add-repository ppa:george-edison55/cmake-3.x -y
+apt-get update 
+apt-get install build-essential qt58base qt58declarative qt58quickcontrols2 qt58graphicaleffects qt58svg qt58creator mesa-common-dev libgl1-mesa-dev pkg-config cmake libnotify-dev --fix-missing -y
