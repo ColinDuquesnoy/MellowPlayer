cfcfeee96ab1d81e3f9fd842732a98dee3b671aa
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-05-06 15:00:39 +0200

    GitlabCI: source qt env

--- scripts/env-setup/linux/ubuntu-16.04-env-setup.sh
+++ scripts/env-setup/linux/ubuntu-16.04-env-setup.sh
@@ -4,10 +4,12 @@ apt-get update
 apt-get install software-properties-common --fix-missing -y
 apt-add-repository ppa:beineri/opt-qt571-xenial -y
 apt-get update
-apt-get install build-essential qt57base qt57declarative qt57quickcontrols2 qt57graphicaleffects qt57svg qt57creator mesa-common-dev libgl1-mesa-dev pkg-config cmake libnotify-dev --fix-missing -y
+apt-get install build-essential qt57base qt57declarative qt57quickcontrols2 qt57graphicaleffects qt57svg qt57creator mesa-common-dev libgl1-mesa-dev pkg-config cmake libnotify-dev xvfb --fix-missing -y
 
 # tool for code coverage reporting
 tar -xvf 3rdparty/lcov-1.13.tar.gz
 pushd lcov-1.13
 make PREFIX="/usr" install
 popd
+
+source /opt/qt57/bin/qt57-env.sh
