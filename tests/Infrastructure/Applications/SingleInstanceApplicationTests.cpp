f89ae48ac25a51cc723fe544251fa3d073508f53
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-05-06 17:46:07 +0200

    Debugging segfault when running tests randomly
    Revert previous commit and remove mpris/dbus related tests

--- tests/Infrastructure/Applications/SingleInstanceApplicationTests.cpp
+++ tests/Infrastructure/Applications/SingleInstanceApplicationTests.cpp
@@ -12,13 +12,13 @@ TEST_CASE("SingleInstanceApplication", "[IntegrationTest]") {
 
     instance1.run();
     Verify(Method(appMock1, run)).Exactly(1);
-    QTest::qSleep(100);
+    QTest::qWait(100);
     Verify(Method(appMock1, initialize)).Exactly(1);
 
     SECTION("Second instance should quit") {
         auto appMock2 = ApplicationMock::get();
         SingleInstanceApplication instance2(appMock2.get());
         instance2.run();
-        QTest::qSleep(300);
+        QTest::qWait(300);
     }
 }
