91b07f5a176a7440cb87204cea1bedac6e72edf1
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-05-28 18:42:14 +0200

    Update archlinux-env-setup.sh

--- scripts/env-setup/linux/archlinux-env-setup.sh
+++ scripts/env-setup/linux/archlinux-env-setup.sh
@@ -2,8 +2,3 @@
 
 pacman --noconfirm --quiet -Syyu
 pacman --noconfirm --quiet -S base-devel qt5-quickcontrols2 qt5-webengine qt5-svg cmake git xorg-server-xvfb libnotify
-
-tar -xvf 3rdparty/lcov-1.13.tar.gz
-pushd lcov-1.13
-make PREFIX="/usr" install
-popd
