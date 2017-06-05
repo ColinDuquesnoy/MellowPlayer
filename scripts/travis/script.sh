13dfda9c1f4c23b4c74a5bfca7c545ca16216322
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-06-05 19:20:11 +0200

    Travis: fix bash syntax error
    and make all travis scripts executable

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ -n "$TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" == "release" ]]; then
+if [ -n "$TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" == "release" ]; then
     ./scripts/travis/full-build.sh;
 elif [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
     ./scripts/travis/trigger-full-build.sh;
