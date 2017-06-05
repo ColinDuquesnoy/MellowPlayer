d22a59dcecc55c02b10904467ebfc38301a76f2d
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-06-05 19:02:27 +0200

    Fix issues in scripts/travis/script.sh

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -n "TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" == "release" ]; then
-    ./full-build.sh;
+if [[ -n "$TRAVIS_TAG" ] || [ "$TRAVIS_BRANCH" == "release" ]]; then
+    ./scripts/travis/full-build.sh;
 elif [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-    ./trigger-full-build.sh;
+    ./scripts/travis/trigger-full-build.sh;
 else
-    ./commit-build.sh;
+    ./scripts/travis/commit-build.sh;
 fi
