e68815b1729ef39caca0bf9eb12746254a626e4b
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-06-18 14:40:41 +0200

    Use Qt 5.9 on appveyor

--- scripts/build/windows/runtests.bat
+++ scripts/build/windows/runtests.bat
@@ -1,5 +1,5 @@
 cd ..\..\..\build\Publish\bin
-set QTDIR=C:\Qt\5.8\msvc2015_64
+set QTDIR=C:\Qt\5.9\msvc2015_64
 set PATH=%QTDIR%\bin;%QTDIR%\lib;%PATH%
 MellowPlayer.UnitTests.exe
 MellowPlayer.IntegrationTests.exe
