81378c1415deb8ff7bbc21978edbf9a30001412d
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-06-26 22:00:59 +0200

    Spotify fix wrong artist name

--- plugins/spotify/integration.js
+++ plugins/spotify/integration.js
@@ -64,7 +64,7 @@ function getPlaybackStatus() {
 }
 
 function getArtist() {
-    return document.querySelector("#main > div > div.nowPlayingBar-container > footer > div > div.now-playing-bar__left > div > div > div.track-info__name > div").children[0].text;
+    return document.querySelector("#main > div > div.nowPlayingBar-container > footer > div.now-playing-bar > div.now-playing-bar__left > div > div > div.track-info__artists").children[0].children[0].children[0].text
 }
 
 function getSongTitle() {
