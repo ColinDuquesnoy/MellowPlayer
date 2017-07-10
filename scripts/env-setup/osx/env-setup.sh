d8df294c97b71382ef57d8a5057153da88ce214f
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-10 23:19:56 +0200

    Switch download to wget on OSX
    curl randomly times out when downloading the qt5 binaries.

--- scripts/env-setup/osx/env-setup.sh
+++ scripts/env-setup/osx/env-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -L --retry 5 -o qt.zip https://www.dropbox.com/s/il5hin8teaf5liu/qt-5.9.0-osx.zip?dl=1
+wget --retry-connrefused --waitretry=1 -O qt.zip "https://www.dropbox.com/s/il5hin8teaf5liu/qt-5.9.0-osx.zip?dl=1"
 unzip -qq qt.zip
 mv clang_64/ qt/
