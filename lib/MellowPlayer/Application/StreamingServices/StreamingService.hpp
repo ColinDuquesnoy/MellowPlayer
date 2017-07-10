f651e2195299bf68a96524486ddc2cc56811f5e6
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-10 16:55:59 +0200

    Try using nested namespaces declaration syntax
    Require gcc6 on travis

--- lib/MellowPlayer/Application/StreamingServices/StreamingService.hpp
+++ lib/MellowPlayer/Application/StreamingServices/StreamingService.hpp
@@ -8,41 +8,40 @@
 #include "StreamingServiceMetadata.hpp"
 #include "StreamingServiceStyle.hpp"
 
-BEGIN_MELLOWPLAYER_NAMESPACE(Application)
+namespace MellowPlayer::Application {
 
-class StreamingServiceScript;
+    class StreamingServiceScript;
 
-class StreamingService: public QObject {
-    Q_OBJECT
-    Q_PROPERTY(QString author READ getAuthor CONSTANT)
-    Q_PROPERTY(QString authorWebsite READ getAuthorWebsite CONSTANT)
-    Q_PROPERTY(QString logo READ getLogo CONSTANT)
-    Q_PROPERTY(QString name READ getName CONSTANT)
-    Q_PROPERTY(QString url READ getUrl CONSTANT)
-    Q_PROPERTY(QString version READ getVersion CONSTANT)
-    Q_PROPERTY(StreamingServiceScript* script READ getScript CONSTANT)
-public:
-    StreamingService(const StreamingServiceMetadata& metadata, const StreamingServiceStyle& style=StreamingServiceStyle());
-    ~StreamingService();
+    class StreamingService : public QObject {
+        Q_OBJECT
+        Q_PROPERTY(QString author READ getAuthor CONSTANT)
+        Q_PROPERTY(QString authorWebsite READ getAuthorWebsite CONSTANT)
+        Q_PROPERTY(QString logo READ getLogo CONSTANT)
+        Q_PROPERTY(QString name READ getName CONSTANT)
+        Q_PROPERTY(QString url READ getUrl CONSTANT)
+        Q_PROPERTY(QString version READ getVersion CONSTANT)
+        Q_PROPERTY(StreamingServiceScript* script READ getScript CONSTANT)
+    public:
+        StreamingService(const StreamingServiceMetadata& metadata,
+                         const StreamingServiceStyle& style = StreamingServiceStyle());
+        ~StreamingService();
 
-    bool isValid() const;
-    const QString& getAuthor() const;
-    const QString& getAuthorWebsite() const;
-    QString getLogo() const;
-    const QString& getName() const;
-    QString getUrl() const;
-    const QString& getVersion() const;
-    StreamingServiceScript* getScript() const;
-    const StreamingServiceStyle& getStyle() const;
+        bool isValid() const;
+        const QString& getAuthor() const;
+        const QString& getAuthorWebsite() const;
+        QString getLogo() const;
+        const QString& getName() const;
+        QString getUrl() const;
+        const QString& getVersion() const;
+        StreamingServiceScript* getScript() const;
+        const StreamingServiceStyle& getStyle() const;
 
-    bool operator==(const StreamingService& rhs) const;
-    bool operator!=(const StreamingService& rhs) const;
+        bool operator==(const StreamingService& rhs) const;
+        bool operator!=(const StreamingService& rhs) const;
 
-private:
-    StreamingServiceMetadata metadata;
-    StreamingServiceStyle style;
-    std::unique_ptr<StreamingServiceScript> script;
-};
-
-END_MELLOWPLAYER_NAMESPACE
-
+    private:
+        StreamingServiceMetadata metadata;
+        StreamingServiceStyle style;
+        std::unique_ptr<StreamingServiceScript> script;
+    };
+}
