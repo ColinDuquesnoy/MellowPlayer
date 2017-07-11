7fbb61e8bd5cb9d8c78d39cdd9a46344046def3f
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-11 18:36:53 +0200

    #59 Add Tidal plugin
    Close #59

---
+++ plugins/tidal/integration.js
@@ -0,0 +1,104 @@
+function getHashCode(s) {
+    return s.split("").reduce(function(a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0);
+        return a & a
+    }, 0);
+}
+
+function toMilliseconds(string) {
+    try {
+        var dtimes = string.split(":");
+        var dminutes = dtimes[0];
+        var dseconds = dtimes[1];
+        var duration = parseInt(dseconds, 10) + (parseInt(dminutes, 10) * 60);
+    } catch (e) {
+        var duration = 0;
+    }
+
+    return duration
+}
+
+function getButtons() {
+    return {
+        "pause": document.getElementsByClassName("play-controls__pause")[0],
+        "play": document.getElementsByClassName("play-controls__play")[0],
+        "next": document.getElementsByClassName("play-controls__next")[0],
+        "previous": document.getElementsByClassName("play-controls__previous")[0],
+        "main": document.getElementsByClassName("play-controls__main-button")[0]
+    }
+}
+
+function isPaused() {
+    return document.getElementsByClassName("play-controls__main-button--paused").length !== 0;
+}
+
+function isPlayerVisible() {
+    return document.getElementsByClassName("player--audio").length > 0;
+}
+
+function update() {
+    console.error("up");
+
+    var results = {
+        "playbackStatus": mellowplayer.PlaybackStatus.STOPPED,
+        "canSeek": false,
+        "canGoNext": false,
+        "canGoPrevious": false,
+        "canAddToFavorites": false,
+        "volume": 1,
+        "duration": 0,
+        "position": 0,
+        "songId": 0,
+        "songTitle": '',
+        "artistName": '',
+        "albumTitle": '',
+        "artUrl": '',
+        "isFavorite": false
+    };
+
+    if (isPlayerVisible()) {
+        results.playbackStatus = isPaused() ? mellowplayer.PlaybackStatus.PAUSED : mellowplayer.PlaybackStatus.PLAYING;
+        results.canGoNext = !getButtons().next.disabled;
+        results.canGoPrevious = !getButtons().previous.disabled;
+        results.songTitle = document.getElementsByClassName("player__text")[0].children[0].innerText;
+        results.artistName = document.getElementsByClassName("player__text")[0].children[1].innerText;
+        results.songId = getHashCode(results.songTitle);
+        results.position = toMilliseconds(document.getElementsByClassName("player__elapsed-time__progress")[0].innerText);
+        results.duration = toMilliseconds(document.getElementsByClassName("player__elapsed-time__duration")[0].innerText);
+        results.artUrl = document.getElementsByClassName("player__image-container")[0].children[0].src;
+    }
+
+    return results;
+}
+
+function play() {
+    getButtons().play.click();
+}
+
+function pause() {
+    getButtons().pause.click();
+}
+
+function goNext() {
+    getButtons().next.click();
+}
+
+function goPrevious() {
+    getButtons().previous.click();
+}
+
+function setVolume(volume) {
+
+}
+
+function addToFavorites() {
+
+}
+
+function removeFromFavorites() {
+
+}
+
+function seekToPosition(position) {
+
+}
