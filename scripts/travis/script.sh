fa184a40c285ef4b6222716558cc495c0cb71409
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-11 19:15:04 +0200

    Travis: perform full build for build triggered through the API

--- scripts/travis/script.sh
+++ scripts/travis/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -n "$TRAVIS_TAG" ]] || [[ "$TRAVIS_BRANCH" == release* ]] || [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
+if [[ -n "$TRAVIS_TAG" ]] || [[ "$TRAVIS_BRANCH" == release* ]] || [[ "$TRAVIS_EVENT_TYPE" == "cron" ]] || [[ "$TRAVIS_EVENT_TYPE" == "api" ]]; then
     ./scripts/travis/full-build.sh;
 else
     ./scripts/travis/commit-build.sh;
