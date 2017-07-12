831e2e6879b3f46302c351176c5e32c7d5c39174
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-12 14:24:21 +0200

    Add basic functional tests
    Starts MellowPlayer executable two times (without a startup service and with a startup service).

--- scripts/travis/commit-build.sh
+++ scripts/travis/commit-build.sh
@@ -9,10 +9,12 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo pip install cpp-coveralls;
   ./scripts/build/linux/build.sh --debug --coverage;
   catchsegv ./scripts/build/linux/runtests.sh --debug;
+  ./scripts/build/functional-tests.sh --debug;
   coveralls --exclude /opt --exclude /usr  --exclude tests/ --exclude 3rdparty -E '.*qrc_.*' -E '.*moc_.*'  -E '.*cmake-build-.*' -E '.*QQmlObjectListModel.*' -b 'cmake-build-debug' -r '.';
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   ./scripts/build/osx/build.sh --release;
   ./scripts/build/osx/runtests.sh --release;
+  ./scripts/build/functional-tests.sh --release;
 fi
