06f6ab3be3e188c05e355d8bc14b58a79233b4f7
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2017-07-28 22:44:41 +0200

    Travis CI: explicitely install python3

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     source ./scripts/env-setup/linux/ubuntu-14.04-env-setup.sh;
-    sudo apt-get install python
+    sudo apt-get install python3
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
