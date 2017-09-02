c9af9284dfbdad44a090aa5d4bcf3394dfcef581
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2017-09-03 00:54:28 +0200

    Add a status message for when ccache build are enabled

--- scripts/cmake/FindDependencies.cmake
+++ scripts/cmake/FindDependencies.cmake
@@ -37,6 +37,7 @@ endif()
 
 find_program(CCACHE_FOUND ccache)
 if(CCACHE_FOUND)
+    message(STATUS "ccache enabled")
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
     set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
 endif(CCACHE_FOUND)
