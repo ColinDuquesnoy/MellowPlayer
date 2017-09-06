624e68216d7687faef093ab63b2afe681b5c52f6
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2017-09-06 20:52:08 +0200

    Use cotire instead

--- scripts/cmake/CMakePCHCompiler/Platform/Linux-Intel-CXXPCH.cmake
+++
@@ -1,3 +0,0 @@
-include(Platform/Linux-Intel)
-__linux_compiler_intel(CXXPCH)
-set(CMAKE_INCLUDE_SYSTEM_FLAG_CXXPCH "-isystem ")
--- scripts/cmake/CMakePCHCompiler/Platform/Windows-Intel-CPCH.cmake
+++
@@ -1,3 +0,0 @@
-include(Platform/Windows-Intel)
-set(_COMPILE_CPCH " /TP")
-__windows_compiler_intel(CPCH)
