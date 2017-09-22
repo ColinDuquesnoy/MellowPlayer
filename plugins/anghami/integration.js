269e50b5137d9de202d0242972815ea2dd0b813c
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2017-09-22 03:46:39 +0200

    Add song position, duration and id to Anghami

--- plugins/anghami/integration.js
+++ plugins/anghami/integration.js
@@ -8,15 +8,49 @@ function update() {
         "canGoPrevious": true,
         "canAddToFavorites": false,
         "volume": 1,
-        "duration": 0,
-        "position": 0,
-        "songId": 0,
+        "duration": getDuration(),
+        "position": getPosition(),
+        "songId": getSongId(),
         "songTitle": getTitle(),
         "artistName": getArtist(),
         "albumTitle": '',
         "artUrl": getAlbumArt(),
         "isFavorite": false
     };
+}
+
+function toSeconds(string) {
+    try {
+        var dtimes = string.split(":");
+        var dminutes = dtimes[0];
+        var dseconds = dtimes[1];
+        var duration = parseInt(dseconds, 10) + (parseInt(dminutes, 10) * 60);
+    } catch (e) {
+        var duration = 0;
+    }
+    return duration
+}
+
+function getHashCode(s) {
+    return s.split("").reduce(function(a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0);
+        return a & a
+    }, 0);
+}
+
+function getDuration() {
+    var remaining = $('#player .duration').innerText.substr(2);
+    var position = $('#player .position').innerText;
+    return toSeconds(remaining) + toSeconds(position)
+}
+
+function getPosition() {
+    var position = $('#player .position').innerText;
+    return toSeconds(duration)
+}
+
+function getSongId(argument) {
+   return getHashCode(getTitle());
 }
 
 function getAlbumArt() {
