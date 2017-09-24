1f3dd3867bea5f5380a135dedbf33a2669580977
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2017-09-24 11:43:59 +0200

    #69 Add UserScriptViewModel

---
+++ lib/MellowPlayer/Presentation/ViewModels/UserScripts/UserScriptViewModel.cpp
@@ -0,0 +1,40 @@
+#include "UserScriptViewModel.hpp"
+#include <MellowPlayer/Application/UserScripts/IUserScript.hpp>
+
+using namespace MellowPlayer::Application;
+using namespace MellowPlayer::Presentation;
+
+UserScriptViewModel::UserScriptViewModel(IUserScript& model, QObject* parent):
+    QObject(parent), model_(model)
+{
+
+}
+
+QString UserScriptViewModel::name() const
+{
+    return model_.name();
+}
+
+void UserScriptViewModel::setName(const QString& name)
+{
+    Q_UNUSED(name);
+    if (name != model_.name()) {
+        model_.setName(name);
+        emit nameChanged();
+    }
+}
+
+QString UserScriptViewModel::code() const
+{
+    return model_.code();
+}
+
+QString UserScriptViewModel::path() const
+{
+    return model_.path();
+}
+
+IUserScript& UserScriptViewModel::model()
+{
+    return model_;
+}
--- tests/UnitTests/Application/UserScripts/FakeUserScript.hpp
+++ tests/UnitTests/Application/UserScripts/FakeUserScript.hpp
@@ -10,7 +10,16 @@ namespace MellowPlayer::Application::Tests
     class FakeUserScript: public UserScriptBase
     {
     public:
-        bool import(const QString& path) override;
-        bool load(const QString& path) override;
+        bool import(const QString& path) override
+        {
+            return load(path);
+        }
+
+        bool load(const QString& path) override
+        {
+            UserScriptBase::path_ = path;
+            Application::UserScriptBase::code_ = FAKE_USER_SCRIPT_CODE;
+            return true;
+        }
     };
 }
