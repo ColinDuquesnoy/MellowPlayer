7e844a2d9479b4385dd5dc248dec7ffe9deadf0a
Rasmus Thomsen <Rasmus.thomsen@protonmail.com>
2017-10-20 19:50:58 +0200

    Plex plugin: initial bring up
    Currently everything but the media keys and art are working

---
+++ plugins/plex/integration.js
@@ -0,0 +1,75 @@
+function update() {
+    var controlClassName = document.getElementsByClassName('AudioVideoPlayerControls-controls-OwK1f')[0];
+    // if controlClassName is undefined the audioplayer isn't opened yet
+    if (!controlClassName)
+        return;
+    var playbackStatus = mellowplayer.PlaybackStatus.STOPPED;
+    if (document.querySelector('[aria-label=Pause]') !== null)
+        playbackStatus = mellowplayer.PlaybackStatus.PLAYING;
+    else if (document.querySelector('[aria-label=Play]') !== null)
+        playbackStatus = mellowplayer.PlaybackStatus.PAUSED;
+    try {
+        var artUrl = document.getElementsByClassName('AudioVideoFullMusic-card-3HHZD MetadataPosterCard-card-3bztR')[
+            0].children[0].children[0].children[0].style["background-image"];
+        artUrl = artUrl.replace('url("', "").replace('")', "");
+    } catch (e) {
+        var artUrl = '';
+    }
+    try {
+        var songTitle = document.getElementsByTagName('title')[0].innerHTML;
+    } catch (e) {
+        var songTitle = '';
+    }
+    return {
+        "playbackStatus": playbackStatus,
+        "canSeek": false,
+        "canGoNext": true,
+        "canGoPrevious": true,
+        "canAddToFavorites": false,
+        "volume": 1,
+        "duration": 0,
+        "position": 0,
+        "songId": 0,
+        "songTitle": songTitle,
+        "artistName": '',
+        "albumTitle": '',
+        "artUrl": artUrl,
+        "isFavorite": false
+    }
+}
+
+function clickButton(buttonName) {
+    document.querySelector("[aria-label=" + buttonName + "]").click()
+}
+
+function play() {
+    clickButton('Play');
+}
+
+function pause() {
+    play();
+}
+
+function goNext() {
+    clickButton('Next');
+}
+
+function goPrevious() {
+    clickButton('Previous');
+}
+
+function setVolume(volume) {
+    // not supported
+}
+
+function addToFavorites() {
+    // not supported
+}
+
+function removeFromFavorites() {
+    // not supported
+}
+
+function seekToPosition(position) {
+    // not supported
+}
