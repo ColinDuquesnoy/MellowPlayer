e63308004a60aa8f218dfb8ec0c549bfb0c4c3ce
Rasmus Thomsen <Rasmus.thomsen@protonmail.com>
2017-10-23 23:32:11 +0200

    Plex plugin: fix media keys

--- plugins/plex/integration.js
+++ plugins/plex/integration.js
@@ -1,3 +1,40 @@
+//-----------------------------------------------------------------------------
+//
+// This file is part of MellowPlayer.
+//
+// MellowPlayer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MellowPlayer is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MellowPlayer.  If not, see <http://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+// Based in part on utils.js of Nuvolaruntime, which is:
+//     Copyright 2014-2017 Jiří Janoušek <janousek.jiri@gmail.com> under the terms of BSD-2:
+//     Redistribution and use in source and binary forms, with or without modification,
+//     are permitted provided that the following conditions are met:
+//     1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//     2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//     THIS SOFTWARE IS PROVIDED BY DMITRY VYUKOV "AS IS" AND ANY EXPRESS OR IMPLIED
+//     WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//     MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//     SHALL DMITRY VYUKOV OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//     LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+//     OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//     LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+//     OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//     ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 function update() {
     var controlClassName = document.getElementsByClassName('AudioVideoPlayerControls-controls-OwK1f')[0];
     // if controlClassName is undefined the audioplayer isn't opened yet
@@ -10,8 +47,8 @@ function update() {
         playbackStatus = mellowplayer.PlaybackStatus.PAUSED;
     try {
         var artBlobUrl = document.getElementsByClassName('AudioVideoPlayerControls-buttonGroupLeft-3kwFX')[
-        0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
-        .style["background-image"];
+            0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0]
+            .style["background-image"];
         artBlobUrl = artBlobUrl.replace('url("', "").replace('")', "");
         // Get the data URL of the blob
         var request = new XMLHttpRequest();
@@ -20,7 +57,7 @@ function update() {
         request.onload = function() {
             var reader = new FileReader();
             reader.readAsDataURL(request.response);
-            reader.onload =  function(e){
+            reader.onload = function(e) {
                 return artUrl = e.target.result;
             };
         };
@@ -52,7 +89,34 @@ function update() {
 }
 
 function clickButton(buttonName) {
-    document.querySelector("[aria-label=" + buttonName + "]").click()
+    // Taken from: https://github.com/tiliado/nuvolaruntime/blob/master/src/mainjs/utils.js
+    function triggerMouseEvent(elm, name, x, y) {
+        var rect = elm.getBoundingClientRect();
+        var width = rect.width * (x === undefined ? 0.5 : x);
+        var height = rect.height * (y === undefined ? 0.5 : y);
+        var opts = {
+            view: document.defaultView,
+            bubbles: true,
+            cancelable: true,
+            button: 0,
+            relatedTarget: elm
+        }
+        opts.clientX = rect.left + width;
+        opts.clientY = rect.top + height;
+        opts.screenX = window.screenX + opts.clientX;
+        opts.screenY = window.screenY + opts.clientY;
+        var event = new MouseEvent(name, opts);
+        elm.dispatchEvent(event);
+    }
+
+    function clickOnElement(elm, x, y) {
+        triggerMouseEvent(elm, 'mouseover', x, y);
+        triggerMouseEvent(elm, 'mousedown', x, y);
+        triggerMouseEvent(elm, 'mouseup', x, y);
+        triggerMouseEvent(elm, 'click', x, y);
+    }
+
+    clickOnElement(document.querySelector("[aria-label=" + buttonName + "]"))
 }
 
 function play() {
@@ -60,7 +124,7 @@ function play() {
 }
 
 function pause() {
-    play();
+    clickButton('Pause');
 }
 
 function goNext() {
