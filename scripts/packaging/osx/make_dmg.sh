1e59c3502b5ada6ac23feab1743d230020935d7a
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2017-12-28 20:11:33 +0100

    Fix path issues on CI due to latest project structure refactoring

--- scripts/packaging/osx/make_dmg.sh
+++ scripts/packaging/osx/make_dmg.sh
@@ -4,17 +4,17 @@ echo "Copying plugins to .app"
 
 build_dir=build
 
-mkdir -p ${build_dir}/src/MellowPlayer.app/Contents/PlugIns/services
-mkdir -p ${build_dir}/src/MellowPlayer.app/Contents/Resources
+mkdir -p ${build_dir}/src/main/MellowPlayer.app/Contents/PlugIns/services
+mkdir -p ${build_dir}/src/main/MellowPlayer.app/Contents/Resources
 
-cp -R src/plugins/ ${build_dir}/src/MellowPlayer.app/Contents/PlugIns/services
-cp scripts/packaging/osx/mellowplayer.icns ${build_dir}/src/MellowPlayer.app/Contents/Resources
-cp scripts/packaging/osx/Info.plist ${build_dir}/src/MellowPlayer.app/Contents
+cp -R src/plugins/ ${build_dir}/src/main/MellowPlayer.app/Contents/PlugIns/services
+cp scripts/packaging/osx/mellowplayer.icns ${build_dir}/src/main/MellowPlayer.app/Contents/Resources
+cp scripts/packaging/osx/Info.plist ${build_dir}/src/main/MellowPlayer.app/Contents
 
-mkdir -p  ${build_dir}/src/MellowPlayer.app/Contents/Resources/qml/QtQuick/
-cp -R /usr/local/opt/qt/qml/QtQuick/Controls ${build_dir}/src/MellowPlayer.app/Contents/Resources/qml/QtQuick/
+mkdir -p  ${build_dir}/src/main/MellowPlayer.app/Contents/Resources/qml/QtQuick/
+cp -R /usr/local/opt/qt/qml/QtQuick/Controls ${build_dir}/src/main/MellowPlayer.app/Contents/Resources/qml/QtQuick/
 
-/usr/local/opt/qt/bin/macdeployqt ${build_dir}/src/MellowPlayer.app -dmg -qmldir=$PWD/src -verbose=3
+/usr/local/opt/qt/bin/macdeployqt ${build_dir}/src/main/MellowPlayer.app -dmg -qmldir=$PWD/src -verbose=3
 
 mkdir -p dist
-cp ${build_dir}/src/MellowPlayer.dmg dist/
+cp ${build_dir}/src/main/MellowPlayer.dmg dist/
--- scripts/packaging/windows/make_installer.bat
+++ scripts/packaging/windows/make_installer.bat
@@ -6,10 +6,10 @@ set BUILD_DIR=build
 
 mkdir %BUILD_DIR%\bin
 mkdir %BUILD_DIR%\bin\plugins
-xcopy /S /C /Y %BUILD_DIR%\src\Release\MellowPlayer.exe %BUILD_DIR%\bin
+xcopy /S /C /Y %BUILD_DIR%\src\main\Release\MellowPlayer.exe %BUILD_DIR%\bin
 xcopy /S /E /D /C /Y src\plugins %BUILD_DIR%\bin\plugins
 
-%QTDIR%\bin\windeployqt %BUILD_DIR%\bin\MellowPlayer.exe -qmldir=src\imports
+%QTDIR%\bin\windeployqt %BUILD_DIR%\bin\MellowPlayer.exe -qmldir=src
 
 "C:\Program Files (x86)\Inno Setup 5\iscc" /Q %BUILD_DIR%/setup.iss
 
