8a108b916e6833fe8aae3e1dd3b0fae460e6f157
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-01-29 17:44:38 +0100

    Fix window not showing on OSX

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,9 +7,7 @@ int main(int argc, char** argv)
     // Init resources embedded in static libraries
     Q_INIT_RESOURCE(infrastructure);
     Q_INIT_RESOURCE(presentation);
-#ifdef USE_IMPORTS_QRC
     Q_INIT_RESOURCE(imports);
-#endif
 
     return Program::main(argc, argv);
 }
