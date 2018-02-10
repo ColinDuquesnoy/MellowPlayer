2e9fc489ffdc6f42d8003170956e6f1392d1f53b
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-02-10 13:30:04 +0100

    Switch to gcc8 on travis

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export CXX="g++-7" CC="gcc-7";
+    export CXX="g++-8" CC="gcc-8";
     source ./scripts/env-setup/linux/ubuntu-14.04-env-setup.sh;
     sudo apt-get install python
 fi
