6c83f855b8aeae02f6ab740018dabc11ea8edcb5
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-02-18 12:27:20 +0100

    Fix appveyor init script

--- scripts/appveyor/init.bat
+++ scripts/appveyor/init.bat
@@ -3,4 +3,6 @@ set PATH=%QTDIR%\bin;%QTDIR%\lib;C:\Qt\Tools\QtCreator\bin;%PATH%
 qbs-setup-toolchains --detect
 qbs-setup-qt --detect
 qbs config profiles.qt-5-10-0.baseProfile MSVC2017-x64
+qbs config defaultProfile qt-5-10-0
+echo "qbs config:"
 qbs config --list
