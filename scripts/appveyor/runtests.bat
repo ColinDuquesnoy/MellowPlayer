1e0f0b5b2cfe0fe4562db4816bcc3d1d8122176c
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-02-25 17:56:47 +0100

    AppVeyor fix runtests script

--- scripts/appveyor/runtests.bat
+++ scripts/appveyor/runtests.bat
@@ -1,5 +1,18 @@
 @echo off
-cd ..\..\build
+cd ..\..\build\release\install-root\tests 
 set QTDIR=C:\Qt\5.10.0\msvc2017_64
 set PATH=%QTDIR%\bin;%QTDIR%\lib;%PATH%
-qbs build -f ../ -p tests release projects.MellowPlayer.buildTests:true
+
+set "success=true" 
+for /r "." %%a in (*.exe) do ( 
+    echo "%%~fa" 
+    call "%%~fa" 
+    if errorlevel 1 ( 
+      set "success=false" 
+    ) 
+) 
+if "%success%" == "true" ( 
+    exit /b 0 
+) else ( 
+    exit /b 1 
+)
