4a7a85b46fb45b12dafc729825d4e52ead809e7e
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2018-02-25 15:45:57 +0100

    build qbs test runner on windows

--- scripts/appveyor/runtests.bat
+++ scripts/appveyor/runtests.bat
@@ -1,17 +1,6 @@
 @echo off
-cd ..\..\build\release\install-root\tests
+cd ..\..\build
 set QTDIR=C:\Qt\5.10.0\msvc2017_64
 set PATH=%QTDIR%\bin;%QTDIR%\lib;%PATH%
-set "success=true"
-for /r "." %%a in (*.exe) do (
-	echo "%%~fa"
-	call "%%~fa"
-	if errorlevel 1 (
-      set "success=false"
-	)
-)
-if "%success%" == "true" (
-	exit /b 0
-) else (
-	exit /b 1
-)
+qbs build -f ../ -p tests release projects.MellowPlayer.buildTests:true
+pause
